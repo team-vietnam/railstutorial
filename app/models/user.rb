3564fc66410621e9164ded539b945706d5916cf4
Cong <kongvc@gmail.com>
2017-08-15 19:25:56 +0900

    9.1.1 Remember token digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   attr_accessor :activation_token
   before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
@@ -16,7 +17,13 @@ class User < ApplicationRecord
   end
 
   # Returns a random token.
-  def self.new_token
+  def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
