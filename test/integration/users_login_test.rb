9ae2410e0e682cb43c73d8bccb424ade366d14f0
Cong <kongvc@gmail.com>
2017-08-17 00:55:14 +0900

    9.3 Remember tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:user1)
+    @user = users(:michael)
+    @user.password = 'password1'
   end
 
   test "login with invalid information" do
@@ -16,9 +17,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with valid information followed by logout" do
-    get login_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password1' } }
+    log_in_as(@user, remember_me: '1')
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -35,5 +34,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,18 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: user.password,
+                                          remember_me: remember_me } }
+  end
 end
